60378ef8b485e701477464bbe824f0622e63c59c
audunvennesland <50698298+audunven@users.noreply.github.com>
2021-08-13 11:37:21 +0200

    Mockup transformation of EDEN data

--- src/main/java/no/sintef/transformer/Transformer.java
+++ src/main/java/no/sintef/transformer/Transformer.java
@@ -1,50 +1,155 @@
 package no.sintef.transformer;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
-import dwc.WaterObserved;
-import dwc.WaterQualityObserved;
-import dwc.WeatherObserved;
+import no.sintef.datamodels.WaterObserved;
+import no.sintef.datamodels.WaterQualityObserved;
+import no.sintef.datamodels.WeatherObserved;
+import no.sintef.datamodels.generic.Location;
 
 public class Transformer {
-	
-	public static WaterObserved transformToWaterObserved (String csvInput) throws IOException {
-		
+
+	//test
+	public static void main(String[] args) throws IOException {
+
+		Set<WaterObserved> records = transformToWaterObserved("./files/EDEN.csv");
+
+		System.out.println("Number of records: " + records.size());
+
+		for (WaterObserved wo : records) {
+			System.out.println("Id: " + wo.getId());
+			System.out.println("Date observed: " + wo.getDateObserved());
+			System.out.println("Flow: " + wo.getFlow());
+			System.out.println("Height: " + wo.getHeight());
+			System.out.println("Discharge amount: " + wo.getDischargeAmount());
+			System.out.println("\n");
+		}
+	}
+
+
+
+	public static Set<WaterObserved> transformToWaterObserved (String csvInput) throws IOException {
+
 		WaterObserved wo;
 		Set<WaterObserved> waterObservedData = new HashSet<WaterObserved>();
-		
+
 		BufferedReader br = new BufferedReader(new FileReader(csvInput));
-		
+
 		String line = br.readLine();
 		String[] params = null;
-		
+
+		Location loc;	
+		String measurementType = null;
+		double lat, lon, measurementValue;
+
 		while (line != null) {
+
 			params = line.split(" ");
+
+			lat = Double.parseDouble(params[3]);
+			lon = Double.parseDouble(params[4]);
+			loc = new Location.LocationBuilder()
+					.setLatitude(lat)
+					.setLongitude(lon)
+					.build();
 			
-			wo = new WaterObserved.WaterObservedBuilder()
-					.setId("someId")
-					.setType("WaterObserved")
-					.setSource("EDEN")
-					.setDateObserved(params[2])
-					
-			
-			
+			measurementType = getMeasurementType(params[0]);
+			measurementValue = getMeasurementValue(params[0]);
+
+
+			if (measurementType.equals("flow")) {
+
+
+				wo = new WaterObserved.WaterObservedBuilder()
+						.setId("someId")
+						.setType("WaterObserved")
+						.setSource("EDEN")				
+						.setDateObserved(params[2])
+						.setLocation(loc)
+						.setFlow(measurementValue)
+						.build();
+
+				waterObservedData.add(wo);
+				line = br.readLine();
+
+			} else if (measurementType.equals("height")) {
+
+				wo = new WaterObserved.WaterObservedBuilder()
+						.setId("someId")
+						.setType("WaterObserved")
+						.setSource("EDEN")				
+						.setDateObserved(params[2])
+						.setLocation(loc)
+						.setHeight(measurementValue)
+						.build();
+
+				waterObservedData.add(wo);
+				line = br.readLine();
+
+
+			} else if (measurementType.equals("dischargeAmount")){
+
+				wo = new WaterObserved.WaterObservedBuilder()
+						.setId("someId")
+						.setType("WaterObserved")
+						.setSource("EDEN")				
+						.setDateObserved(params[2])
+						.setLocation(loc)
+						.setDischargeAmount(measurementValue)
+						.build();
+
+				waterObservedData.add(wo);
+				line = br.readLine();
+
+			}
+
 		}
-				
-	}
-	
-	public static WaterQualityObserved transformToWaterQualityObserved (String jsonInput) {
+
+		br.close();
 		
+		return waterObservedData;
+
 	}
-	
-	public static WeatherObserved transformToWeatherObserved (String jsonInput) {
-		
+
+
+
+	//TODO: Just mocking this, get info on how to interpret these values from SIAAP
+	private static String getMeasurementType (String tagValue) {
+
+		String measurementType = null;
+		if (tagValue.contains("_Q_")) {
+			measurementType = "flow";
+		} else if (tagValue.contains("_N_")) {
+			measurementType = "height";
+		} else if (tagValue.contains("_D_")) {
+			measurementType = "dischargeAmount";
+		}
+
+		return measurementType;
+
+
 	}
+	//TODO: Just mocking this, get info on how to interpret these values from SIAAP
+	private static double getMeasurementValue (String tagValue) {
+
+		String value = tagValue.substring(tagValue.indexOf("=")+1, tagValue.length());
+
+		double measurementValue = Double.parseDouble(value.replace(",", "."));
+
+		return measurementValue;
+
+	}
+
+	//	public static WaterQualityObserved transformToWaterQualityObserved (String jsonInput) {
+	//		
+	//	}
+	//	
+	//	public static WeatherObserved transformToWeatherObserved (String jsonInput) {
+	//		
+	//	}
 
 }
